9f0363ffb2be39a733ce028b3630f1380f4ce9c0
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-04 01:10:51 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,12 @@
 class Mp3_importer
-  
+  attr_reader :path, :files
+
+  def initialize(path)
+    @path = path
+    @files = Dir.entries(path).grep(/.*\.mp3/)
+  end
+
+  def import
+    @files.each {|file| Song.new_by_filename(file)}
+  end
 end
