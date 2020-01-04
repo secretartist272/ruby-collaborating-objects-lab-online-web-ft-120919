989f033a406133944d8df526d4307af131da643e
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-04 01:09:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,37 @@
-require 'pry'
+class Artist
+  attr_accessor :name
 
-class Artist 
-  attr_accessor :name 
-  
-  @@song_count = 0 
-  
   @@all = []
-  
+
   def initialize(name)
     @name = name
-    @@all == self
-    @songs = []
+    @songs= []
   end
-  
-  def self.all
-    @@all
-  end
-  
+
   def add_song(song)
     @songs << song
     song.artist = self
+    song
   end
+
+  def songs
+    @songs
+  end
+
+  def save
+    @@all << self
+    self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
+  end
+
+  def print_songs
+    self.songs.each {|song| puts song.name} 
+   end
 end
