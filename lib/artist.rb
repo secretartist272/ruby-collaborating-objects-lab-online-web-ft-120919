d1c19e9cceba5d30cb81fa3de7fdec1523434bed
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-04 01:05:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,22 +9,12 @@ class Artist
   
   def initialize(name)
     @name = name
-    @songs
+    @songs = []
   end
   
   def self.all
     @@all
   end
   
-  def song
-    Song.all.select do |song|
-      song.artist == self.song
-    end
-  end
-  
-   def add_song(song)
-    song.artist = self
-	  @@song_count += 1
-  end
-
+  def
 end
